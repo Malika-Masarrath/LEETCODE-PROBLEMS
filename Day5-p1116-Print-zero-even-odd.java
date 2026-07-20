4839544de0af5674f3ba69aa4b72e348dc5470ab
Shaik-Malika-Masarrath <ayeshashaikalima@gmail.com>
2026-07-20 10:54:48 +0530

    Implement ZeroEvenOdd class with threading

--- Day5-p1116-Print-zero-even-odd.java
+++ Day5-p1116-Print-zero-even-odd.java
@@ -1,1 +1,53 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
 
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zero = new Semaphore(1);
+    private Semaphore even = new Semaphore(0);
+    private Semaphore odd = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                even.release();
+            } else {
+                odd.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+}
+
+// ✅ Driver class with main method
+public class Main {
+    public static void main(String[] args) {
+        ZeroEvenOdd zeo = new ZeroEvenOdd(5);
+        IntConsumer printNumber = x -> System.out.print(x);
+
+        new Thread(() -> { try { zeo.zero(printNumber); } catch (InterruptedException e) {} }).start();
+        new Thread(() -> { try { zeo.even(printNumber); } catch (InterruptedException e) {} }).start();
+        new Thread(() -> { try { zeo.odd(printNumber); } catch (InterruptedException e) {} }).start();
+    }
+}
