0f1a42e8ee3150a66e1e9ec35e47043041712d11
Shaik-Malika-Masarrath <ayeshashaikalima@gmail.com>
2026-07-20 09:05:10 +0530

    Update DAY1-hw-p1295.java

--- DAY1-hw-p1295.java
+++ DAY1-hw-p1295.java
@@ -1,1 +1,22 @@
+lass Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            int digits = digitCount(num);
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    // Helper method to count digits without converting to string
+    private int digitCount(int num) {
+        int digits = 0;
+        while (num > 0) {
+            num /= 10;
+            digits++;
+        }
+        return digits;
+    }
+}
