2c0bd859479c37121d78027dfe8e7a0eceae8a08
Shaik-Malika-Masarrath <ayeshashaikalima@gmail.com>
2026-07-26 10:33:37 +0530

    Implement isAnagram method in Solution class

--- Day6-p65-Valid-Anagram.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int lengthOfLastWord(String s) {
-        s = s.trim(); 
-        int lastSpace = s.lastIndexOf(' '); 
-        return s.length() - lastSpace - 1;  // Step 3: length of last word
-    }
-}
